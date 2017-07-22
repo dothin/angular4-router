b968449c277384840285fc70918e64cbf9f9ea91
gaoHB <1286513426@qq.com>
2017-07-22 23:37:55 +0800

    路由404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StockComponent } from './stock/stock.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: '', component: HomeComponent},
+  {path: 'stock', component: StockComponent},
+  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
