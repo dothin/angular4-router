3267b24d6f9f426eddde345fc58c88f9818c3be0
gaoHB <1286513426@qq.com>
2017-10-07 17:35:22 +0800

    update( project ):change css to scss

--- src/app/code404/code404.component.ts
+++ src/app/code404/code404.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-code404',
   templateUrl: './code404.component.html',
-  styleUrls: ['./code404.component.css']
+  styleUrls: ['./code404.component.scss']
 })
 export class Code404Component implements OnInit {
 
